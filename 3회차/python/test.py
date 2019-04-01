f68179905b2a708ebf30c82234594c57180b35ce
unknown <rlagus0908@naver.com>
2019-04-01 19:33:59 +0900

    19.04.01

---
+++ 3회차/python/test.py
@@ -0,0 +1,77 @@
+# 출력
+print("hello python")
+
+# 정수
+a = 1
+b = 2
+print(a+b)
+
+# 실수
+a1 = 3.5
+b1= 1.2
+print(a1+b1)
+
+# 문자열
+str1 = "졸려"
+str2 = "자고싶다"
+str3 = "지금은 새벽 3시"
+print(str1+str2+str3)
+print(str1 * 5)
+
+
+mystring = "python zz"
+
+print(mystring[0])
+print(mystring[6])
+print(mystring[8])
+print(mystring[-1])
+
+print(len(mystring))
+
+mystring = "hello world"
+
+print(mystring[0:5])
+print(mystring[6:11])
+print(mystring[6:])
+print(mystring[:5])
+print(mystring[6:-1])
+print(mystring[::-1])
+
+# 사칙연산
+print(1 + 2) #더하기
+print(5 - 2) #빼기
+print(2 * 4) #곱하기
+print(4 / 2) #나누기
+print(6 // 4) #몫
+print(6 % 4) #나머지
+print(2 ** 3) #제곱
+
+
+# 입력하는것
+z = input("입력해 봐용 : ")
+print(z)
+print(type(z))
+
+y = int(input("입력해 봐용 : "))
+print(y)
+print(type(z))
+
+
+number = 12
+number_f = float(number)
+number_s = str(number)
+print(type(number))
+print(type(number_f))
+print(type(number_s))
+
+# 오류()
+# string = "abs"
+# string = int(string)
+
+# 비교연산자
+print(3 == 3) #true
+print(3 != 3) #false
+print(3 >= 3) #true
+print(3 <= 3) #true
+print(3 > 3) #false
+print(3 < 3) #false
