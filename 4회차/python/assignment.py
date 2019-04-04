136e3e161b80a6ad7f730981c0b60697bdb9aea0
unknown <rlagus0908@naver.com>
2019-04-05 00:12:09 +0900

    19.04.04

--- 4회차/python/assignment.py
+++ 4회차/python/assignment.py
@@ -11,21 +11,35 @@ class assignment:
 # 리스트로 여러개의 데이터를 저장합니다
 phone_book_list = []
 while(1):
+
+    # name 입력
     name = input('이름을 입력하세요 : ')
+    
     # 그만이 입력된다면 while문을 종료합니다.
     if(name == '그만'):
         break
+    
+    # phone입력
     phone = input('전화번호를 입력하세요 : ')
+    
+    # sex입력
     sex = input('성별을 입력하세요 (male이나 female로 작성해주세요) : ')
     if(sex == 'male'):
         sex = 'male'
+   
     elif(sex == 'female'):
         sex = 'female'
+   
     else:
         sex = 'unknown'
+    
     # 여러개의 생성자를 저장하기위해 리스트에 append시킵니다.
     phone_book_list.append( assignment(name,phone,sex) )
     
     # 리스트에 저장된 생성자의 개수만큼 출력하기위해 for문을 이용하여 출력합니다 
     for a in phone_book_list:
         print(a)
+
+# 그만이 입력된 후 저장된 리스트의 값을 출력합니다
+for a in phone_book_list:
+    print(a)
