0ee037f1e608c22fc8ca648be5839f28c0a7471f
unknown <rlagus0908@naver.com>
2019-04-04 21:57:42 +0900

    19.04.04

---
+++ 3회차/python/assignment.py
@@ -0,0 +1,31 @@
+class assignment:
+    # 생성자(정보저장)
+    def __init__(self,name='',phone='',sex=''):
+        self.name = name
+        self.phone = phone
+        self.sex = sex
+    # 출력
+    def __str__(self):
+        return "이름은 {}, 전화번호는 {}, 성별은 {} 입니다." .format(self.name,self.phone,self.sex)
+
+# 리스트로 여러개의 데이터를 저장합니다
+phone_book_list = []
+while(1):
+    name = input('이름을 입력하세요 : ')
+    # 그만이 입력된다면 while문을 종료합니다.
+    if(name == '그만'):
+        break
+    phone = input('전화번호를 입력하세요 : ')
+    sex = input('성별을 입력하세요 (male이나 female로 작성해주세요) : ')
+    if(sex == 'male'):
+        sex = 'male'
+    elif(sex == 'female'):
+        sex = 'female'
+    else:
+        sex = 'unknown'
+    # 여러개의 생성자를 저장하기위해 리스트에 append시킵니다.
+    phone_book_list.append( assignment(name,phone,sex) )
+    
+    # 리스트에 저장된 생성자의 개수만큼 출력하기위해 for문을 이용하여 출력합니다 
+    for a in phone_book_list:
+        print(a)
--- 3회차/python/test.py
+++ 3회차/python/test.py
@@ -1,77 +1,117 @@
-# 출력
-print("hello python")
+# # 출력
+# print("hello python")
 
-# 정수
-a = 1
-b = 2
-print(a+b)
+# # 정수
+# a = 1
+# b = 2
+# print(a+b)
 
-# 실수
-a1 = 3.5
-b1= 1.2
-print(a1+b1)
+# # 실수
+# a1 = 3.5
+# b1= 1.2
+# print(a1+b1)
 
-# 문자열
-str1 = "졸려"
-str2 = "자고싶다"
-str3 = "지금은 새벽 3시"
-print(str1+str2+str3)
-print(str1 * 5)
-
-
-mystring = "python zz"
-
-print(mystring[0])
-print(mystring[6])
-print(mystring[8])
-print(mystring[-1])
-
-print(len(mystring))
-
-mystring = "hello world"
-
-print(mystring[0:5])
-print(mystring[6:11])
-print(mystring[6:])
-print(mystring[:5])
-print(mystring[6:-1])
-print(mystring[::-1])
-
-# 사칙연산
-print(1 + 2) #더하기
-print(5 - 2) #빼기
-print(2 * 4) #곱하기
-print(4 / 2) #나누기
-print(6 // 4) #몫
-print(6 % 4) #나머지
-print(2 ** 3) #제곱
+# # 문자열
+# str1 = "졸려"
+# str2 = "자고싶다"
+# str3 = "지금은 새벽 3시"
+# print(str1+str2+str3)
+# print(str1 * 5)
 
 
-# 입력하는것
-z = input("입력해 봐용 : ")
-print(z)
-print(type(z))
+# mystring = "python zz"
 
-y = int(input("입력해 봐용 : "))
-print(y)
-print(type(z))
+# print(mystring[0])
+# print(mystring[6])
+# print(mystring[8])
+# print(mystring[-1])
+
+# print(len(mystring))
+
+# mystring = "hello world"
+
+# print(mystring[0:5])
+# print(mystring[6:11])
+# print(mystring[6:])
+# print(mystring[:5])
+# print(mystring[6:-1])
+# print(mystring[::-1])
+
+# # 사칙연산
+# print(1 + 2) #더하기
+# print(5 - 2) #빼기
+# print(2 * 4) #곱하기
+# print(4 / 2) #나누기
+# print(6 // 4) #몫
+# print(6 % 4) #나머지
+# print(2 ** 3) #제곱
 
 
-number = 12
-number_f = float(number)
-number_s = str(number)
-print(type(number))
-print(type(number_f))
-print(type(number_s))
+# # 입력하는것
+# z = input("입력해 봐용 : ")
+# print(z)
+# print(type(z))
 
-# 오류()
-# string = "abs"
-# string = int(string)
+# y = int(input("입력해 봐용 : "))
+# print(y)
+# print(type(z))
 
-# 비교연산자
-print(3 == 3) #true
-print(3 != 3) #false
-print(3 >= 3) #true
-print(3 <= 3) #true
-print(3 > 3) #false
-print(3 < 3) #false
+
+# number = 12
+# number_f = float(number)
+# number_s = str(number)
+# print(type(number))
+# print(type(number_f))
+# print(type(number_s))
+
+# # 오류()
+# # string = "abs"
+# # string = int(string)
+
+# # 비교연산자
+# print(3 == 3) #true
+# print(3 != 3) #false
+# print(3 >= 3) #true
+# print(3 <= 3) #true
+# print(3 > 3) #false
+# print(3 < 3) #false
+
+
+# # 조건문 if
+# people = 3
+# apple = 20
+# if people < apple / 5:
+#     print('신나는 사과 파티! 배터지게 먹자')
+# if apple % people > 0:
+#      print('신나는 사과 맛나나?')
+# if apple % people > 0:
+
+
+# class cal:
+#     def __init__(selt, x=0, y=0):
+#         self.x = x
+#         self.y = y
+
+#     def setdata(self, x, y):
+#         self.x = x
+#         self.y = y
+    
+#     def add1(self):
+#         print(self.x,self.y)
+    
+#     def add2(self):
+#         return x+y
+
+
+# a =cal()
+# a.add1()
+# print(a.add1())
+# a.add2(1,2) 
+i=0
+while (1):
+    name = input('이름을 입력하세요 : ')
+    if(name == '그만'):
+        break
+    i = i + 1
+    
+    print('a' + 'i') 
